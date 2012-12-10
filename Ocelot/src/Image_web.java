f766bfa62e53c7c3eeaaa1b2524607dd6e71e479
Hadrien Delattre <hardquannone@hotmail.fr>
2012-12-10 16:40:31 +0100

    finalisation des constructeurs de Mot et Image_web

--- Ocelot/src/Image_web.java
+++ Ocelot/src/Image_web.java
@@ -1,6 +1,16 @@
+import java.awt.Color;
+import java.awt.Image;
+
 
 public class Image_web extends Element implements MotOuImage {
-	public Image_web(int largeur, int hauteur, int x, int y){
+	private Image image;
+	private String source;
+	private Color bordure;
+	
+	public Image_web(int largeur, int hauteur, int x, int y, Image image, String source, Color bordure){
 		super(largeur, hauteur, x, y);
+		this.image = image;
+		this.source = source;
+		this.bordure = bordure;
 	}
 }
--- Ocelot/src/Mot.java
+++ Ocelot/src/Mot.java
@@ -1,6 +1,21 @@
+import java.awt.Color;
+
 
 public class Mot extends Element implements MotOuImage{
-	public Mot(int largeur, int hauteur, int x, int y){
+	String caracteres;
+	String fonte;
+	boolean graisse;
+	boolean italique;
+	int corps;
+	Color couleur;
+	
+	public Mot(int largeur, int hauteur, int x, int y, String caracteres, String fonte, boolean graisse, boolean italique, int corps, Color couleur){
 		super(largeur, hauteur, x, y);
+		this.caracteres = caracteres;
+		this.fonte = fonte;
+		this.graisse = graisse;
+		this.italique = italique;
+		this.corps = corps;
+		this.couleur = couleur;
 	}
 }
