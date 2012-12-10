9ceb312bc1ac3673764969269ceaeefed6527baa
Hadrien Delattre <hardquannone@hotmail.fr>
2012-12-10 16:12:17 +0100

    ajout d'un constructeur pour Element. Mise à jour du constructeur de tout ses héritiers.

--- Ocelot/src/Image_web.java
+++ Ocelot/src/Image_web.java
@@ -1,4 +1,6 @@
 
 public class Image_web extends Element implements MotOuImage {
-
+	public Image_web(int largeur, int hauteur, int x, int y){
+		super(largeur, hauteur, x, y);
+	}
 }
--- Ocelot/src/Mot.java
+++ Ocelot/src/Mot.java
@@ -1,4 +1,6 @@
 
-public class Mot implements MotOuImage{
-
+public class Mot extends Element implements MotOuImage{
+	public Mot(int largeur, int hauteur, int x, int y){
+		super(largeur, hauteur, x, y);
+	}
 }
