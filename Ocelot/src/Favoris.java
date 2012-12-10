296669705590344c8a67a351dd6b1c26fee73e2a
Hadrien Delattre <hardquannone@hotmail.fr>
2012-12-10 16:55:05 +0100

    implémentation de la classe Favoris

--- Ocelot/src/Favoris.java
+++ Ocelot/src/Favoris.java
@@ -1,4 +1,26 @@
+import java.util.LinkedList;
+
 
 public class Favoris {
+	private LinkedList<URL> urls;
+	
+	/*
+	 * Création d'une nouvelle liste de favoris vide
+	 */
+	public Favoris(){
+		this.urls = new LinkedList<URL>();
+	}
+	
+	/*
+	 * Création d'une nouvelle liste de favoris
+	 * @param u liste de favoris que doit contenir l'objet Favoris
+	 */
+	public Favoris(LinkedList<URL> u){
+		this.urls = u;
+	}
+
+	public LinkedList<URL> getUrls() {
+		return urls;
+	}
 
 }
