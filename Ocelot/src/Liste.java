3a4b8e29c98b632cefb11772f64c52b1acc10324
Hadrien Delattre <hardquannone@hotmail.fr>
2012-12-10 18:21:53 +0100

    Implémenté la classe Liste

--- Ocelot/src/Liste.java
+++ Ocelot/src/Liste.java
@@ -1,6 +1,10 @@
+import java.util.LinkedList;
+
 
 public class Liste extends Element{
-	public Liste(int largeur, int hauteur, int x, int y){
+	LinkedList<Item> items;
+	public Liste(int largeur, int hauteur, int x, int y, LinkedList<Item> i){
 		super(largeur, hauteur, x, y);
+		this.items = i;
 	}
 }
