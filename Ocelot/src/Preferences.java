c96068b6da0790099aa971b81a0f8b1484fddbaf
Hadrien Delattre <hardquannone@hotmail.fr>
2012-12-18 18:16:39 +0100

    Ajout des attributs de la classe Preferences

--- Ocelot/src/Preferences.java
+++ Ocelot/src/Preferences.java
@@ -1,4 +1,49 @@
+import java.awt.Color;
+import java.util.Date;
 
 public class Preferences {
+	static private URL url_accueil;
+	static private URL url_aide;
+	static private Date date_max_historique;
+	static private Color couleur_liens;
+	static private String police_par_defaut;
+	static private Color couleur_police;
+	static private Color couleur_fond;
+		
+	
+	public Preferences(){
+		
+	}
+	
+	public void charger(){
+		
+	}
 
+	public static URL getUrl_accueil() {
+		return url_accueil;
+	}
+
+	public static URL getUrl_aide() {
+		return url_aide;
+	}
+
+	public static Date getDate_max_historique() {
+		return date_max_historique;
+	}
+
+	public static Color getCouleur_liens() {
+		return couleur_liens;
+	}
+
+	public static String getPolice_par_defaut() {
+		return police_par_defaut;
+	}
+
+	public static Color getCouleur_police() {
+		return couleur_police;
+	}
+
+	public static Color getCouleur_fond() {
+		return couleur_fond;
+	}
 }
