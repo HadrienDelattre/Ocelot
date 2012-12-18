86e2399ddbef4a8a77b60188d3afee0ccae2bc48
Hadrien Delattre <hardquannone@hotmail.fr>
2012-12-18 17:48:00 +0100

    implementation de la methode ajouter favoris

--- Ocelot/src/Favoris.java
+++ Ocelot/src/Favoris.java
@@ -1,4 +1,5 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 
 public class Favoris {
@@ -17,6 +18,34 @@ public class Favoris {
 	 */
 	public Favoris(LinkedList<URL> u){
 		this.urls = u;
+	}
+	
+	/*
+	 * Ajoute un favoris à la liste.
+	 * Si le favoris existe déjà dans la liste un est simplement
+	 * mis à jour.
+	 */
+	public void ajouterFavoris(URL urlNouveau){
+		boolean favorisExisteDeja = false;
+
+		// iteration sur tout les favoris
+		ListIterator<URL> urlIter = this.getUrls().listIterator();
+		while(!favorisExisteDeja && urlIter.hasNext()){
+			URL currentUrl = urlIter.next();
+			// si l'url à ajouter existe déjà dans les favoris
+			if(currentUrl.getUrl() == urlNouveau.getUrl()){
+				favorisExisteDeja = true;
+				// actualisation de l'URL
+				currentUrl.setDate(urlNouveau.getDate());
+				currentUrl.setTitre(urlNouveau.getTitre());
+				currentUrl.setStatut(urlNouveau.getStatut());
+			}
+		}
+		// si à la fin de la recherche, l'URL n'a pas été
+		// trouvé parmis les favoris déjà existants ;
+		if(!favorisExisteDeja){
+			this.urls.add(urlNouveau);
+		}
 	}
 
 	public LinkedList<URL> getUrls() {
