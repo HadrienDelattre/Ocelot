9a4fbcf9efff892e1ebb29fbe9719f9e2404c365
Hadrien Delattre <hardquannone@hotmail.fr>
2012-12-27 18:47:16 +0100

    ajout des attributs Favoris et Historique_global dans les fenetres modales

--- Ocelot/src/Fenetre_favoris.java
+++ Ocelot/src/Fenetre_favoris.java
@@ -1,7 +1,9 @@
 public class Fenetre_favoris {
 	  private boolean visibilite;
+	  private Favoris favoris;
 	  
 	  public Fenetre_favoris(){
 		  this.visibilite = false;
+		  this.favoris = new Favoris();
 	  }
 }
--- Ocelot/src/Fenetre_historique_global.java
+++ Ocelot/src/Fenetre_historique_global.java
@@ -1,8 +1,10 @@
 
 public class Fenetre_historique_global {
   private boolean visibilite;
+  private Historique_global historique_global;
   
   public Fenetre_historique_global(){
 	  this.visibilite = false;
+	  this.historique_global = new Historique_global();
   }
 }
